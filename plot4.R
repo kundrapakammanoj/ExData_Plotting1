b8e8483fddccb05e02645088f30707d4db057a18
kundrapakammanoj <49957186+kundrapakammanoj@users.noreply.github.com>
2020-12-22 15:41:43 -0600

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,6 +1,6 @@
 
 library(sqldf)
-powerConsumption<-read.csv.sql("H:/ManojK/Data Science Training/exdata_data_household_power_consumption/household_power_consumption.txt","select * from file where Date= '1/2/2007' or Date = '2/2/2007'",sep=";")
+powerConsumption<-read.csv.sql("./household_power_consumption.txt","select * from file where Date= '1/2/2007' or Date = '2/2/2007'",sep=";")
 
 
 powerConsumption$dateTime<-as.POSIXct(strptime(paste(powerConsumption$Date,powerConsumption$Time,sep =" "), "%d/%m/%Y %H:%M:%S"))
